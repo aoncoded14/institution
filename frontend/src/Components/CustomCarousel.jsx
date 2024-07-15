737f140920c4fd71572564fdb12aa9c1a8e2fa06
aoncoded14 <dorkuma958@gmail.com>
2024-07-15 22:25:06 +0100

    Experimenting with new UI. working on the models and serializers

--- frontend/src/Components/CustomCarousel.jsx
+++ frontend/src/Components/CustomCarousel.jsx
@@ -1,35 +1,9 @@
-import Carousel from 'react-bootstrap/Carousel';
-import '../styles/CustomCarousel.css';
-import welcome from '../assets/svgs/welcome.png';
-import examsbro from '../assets/svgs/examsbro.png';
-import printing1 from '../assets/svgs/printing1.png';
+import React from 'react'
 
 const CustomCarousel = () => {
   return (
-    <Carousel id="home">
-      <Carousel.Item>
-        <img className="d-block w-100" src={welcome} alt="Welcome" />
-        <Carousel.Caption>
-          <h3>Welcome</h3>
-          <p><a className="btn btn-dark" href="#welcome">Go to Welcome</a></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={examsbro} alt="Admissions Now" />
-        <Carousel.Caption>
-          <h3>Admissions Now</h3>
-          <p><a className="btn btn-dark" href="#admissions">Go to Admissions</a></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={printing1} alt="Printing Services" />
-        <Carousel.Caption>
-          <h3>Printing Services</h3>
-          <p><a className="btn btn-dark" href="#printing">Go to Printing Services</a></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
+    <div>CustomCarousel</div>
+  )
 }
 
-export default CustomCarousel;
+export default CustomCarousel
